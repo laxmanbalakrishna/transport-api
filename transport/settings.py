082b137a41507d59abb83722609790bcff8faaa7
laxmanbalakrishna <laxmanbalakrishna.gujjeti@bdnt.tech>
2024-10-03 12:50:56 +0530

    Modified Allowed hosts

--- transport/settings.py
+++ transport/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = secrets['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://transport-api-q7km.onrender.com']
+ALLOWED_HOSTS = ['transport-api-q7km.onrender.com']
+
 
 
 # Application definition
